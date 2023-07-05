59351871465d23238546b73fe401159e16fc7136
wuhibe <wuhibetamire710@gmail.com>
2023-07-05 10:14:45 +0300

    chore: configure prettier and commitlint on project

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,11 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'dashboard')]
-    public function number(): Response
+    #[Route('/', methods: ['GET'], name: 'dashboard')]
+    public function index(LoggerInterface $logger): Response
     {
         return $this->render('index.html.twig');
     }
